ddf60c8d92c3d7541fab89ee6e3cd99d66b71abb
martin <this.frontend@gmail.com>
2015-07-01 07:48:38 +0500

    rename

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 var scripts = [
-  './src/main.js'
+  './src/main.js',
+  './src/core/application.js'
+];
+
+var tryFiles = [
+  './try/index.html',
+  './try/script.js'
 ];
 
 gulp.task('concat', function () {
   return gulp.src(scripts)
-    .pipe(concat('f7k.js'))
+    .pipe(concat('fk.js'))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src(tryFiles)
     .pipe(gulp.dest('build/'));
 });
 
 gulp.task('default', ['concat']);
+gulp.task('try', ['concat', 'copy']);
